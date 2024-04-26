811ca78e1c35cf24d59254af96f32a2c72bf86dc
Andrey Krivetsky <a.krivetsky@syberry.com>
2024-04-26 14:52:44 +0200

    Init project 2

--- src/App.js
+++ src/App.js
@@ -5,18 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              window.dataLayer.push({
+                event: 'LICK_CLICK_EVENT_NAME',
+                where: 'mainpage'
+              })
+            }}
         >
           Learn React
         </a>
+
+        <button
+            className="App-link"
+            onClick={() => {
+              window.dataLayer.push({
+                event: 'ZALUPOVICH',
+                where: 'mainpage'
+              })
+            }}
+        >
+          ZALUPA
+        </button>
       </header>
     </div>
   );
